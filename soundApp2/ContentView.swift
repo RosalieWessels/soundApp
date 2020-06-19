fd3f73604ac02bd0217d66f25bdec8aab081326a
Rosalie Wessels <rosalie.wessels@yahoo.com>
2020-06-19 09:13:11 -0700

    Initial Commit

---
+++ soundApp2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  soundApp2
+//
+//  Created by W Wessels on 6/19/20.
+//  Copyright © 2020 W Wessels. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

f367d691a786f21533441b4714b62606cb99e9e4
Rosalie Wessels <rosalie.wessels@yahoo.com>
2020-06-19 10:22:26 -0700

    added songs and ability to play them

--- soundApp2/ContentView.swift
+++ soundApp2/ContentView.swift
@@ -7,26 +7,56 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    
+    @State var song: AVAudioPlayer?
+    
+    @State var songs = ["Still Corners - Fireflies.mp3", "Alkaline Trio - Clavicle.mp3", "Blackbird Blackbird - Pure.mp3"]
+    
     var body: some View {
         VStack {
             Text("Welcome to Sound app")
             Image("dj").resizable().aspectRatio(contentMode: .fit)
             HStack {
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Button(action: {
+                    self.playSound(soundNumber: 1)
+                }) {
                     Image("record").resizable().aspectRatio(contentMode: .fit)
                 }
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Button(action: {
+                    self.playSound(soundNumber: 2)
+                }) {
                     Image("record").resizable().aspectRatio(contentMode: .fit)
                 }
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Button(action: {
+                    self.playSound(soundNumber: 3)
+                }) {
                     Image("record").resizable().aspectRatio(contentMode: .fit)
                 }
             }
         }
+        
+        
+    }
+    
+    func playSound(soundNumber : Int) {
+        let path = Bundle.main.path(forResource: songs[soundNumber - 1], ofType:nil)!
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            song = try AVAudioPlayer(contentsOf: url)
+            song?.play()
+        } catch {
+            // couldn't load file :(
+        }
     }
 }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

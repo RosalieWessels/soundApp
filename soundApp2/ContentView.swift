bce42d8dfd392d4f3bdf132c9af8df1c734be55b
Rosalie Wessels <rosalie.wessels@yahoo.com>
2020-06-19 09:25:23 -0700

    started Sound App

--- soundApp2/ContentView.swift
+++ soundApp2/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+            Text("Welcome to Sound app")
+        }
     }
 }
 
